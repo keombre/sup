f8603a304e5c771ac21c188f5aaaf212cdbf6bf0
keombre <keombre8@gmail.com>
2018-12-28 16:59:05 +0100

    separate module

--- modules/lists/controller/draw/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace controller\lists\draw;
-
-class api extends \sup\controller {
-    function __invoke($request, $response, $args) {
-    }
-}
--- modules/lists/controller/draw/draw.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace controller\lists\draw;
-
-class draw extends \sup\controller {
-    function __invoke($request, $response, $args) {
-    }
-}
