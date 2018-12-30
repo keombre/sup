d649e220481b8ed744ac4d4c01d563bf8ad024e5
keombre <keombre8@gmail.com>
2018-12-30 02:19:15 +0100

    changeRole lang

--- app/controller/auth/changeRole.php
+++ app/controller/auth/changeRole.php
@@ -18,13 +18,21 @@ class changeRole extends \sup\controller {
             $role = intVal(filter_var(@$data['role'], FILTER_SANITIZE_STRING));
             $oldRole = $this->container->auth->user->getInfo('activeRole');
 
+            $l = $this->container->lang;
+
             if (!$this->container->auth->changeRole($role))
-                $this->redirectWithMessage($response, 'user-changeRole', "error", ["Chyba!", "Neexistující role"]);
+                $this->redirectWithMessage($response, 'user-changeRole', "error", [
+                    $l->g('error-notfound-title', 'user-changeRole'),
+                    $l->g('error-notfound-message', 'user-changeRole')
+                ]);
 
             else if ($oldRole == $role)
                 return $response->withRedirect($this->container->router->pathFor('dashboard'), 301);
             else
-                $this->redirectWithMessage($response, 'dashboard', "status", ["Úspěch", "Nyní jste " . strtolower($this->container->lang->g($role, 'roles'))]);
+                $this->redirectWithMessage($response, 'dashboard', "status", [
+                    $l->g('success-change-title', 'user-changeRole'),
+                    $l->g('success-change-message', 'user-changeRole') . strtolower($this->container->lang->g($role, 'roles'))
+                ]);
         }
         return $response;
     }
